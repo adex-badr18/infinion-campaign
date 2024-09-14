983d6986aacfc951072137ea6509443f51a30ed3
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-09-14 10:28:35 +0100

    feat: Render button & link on side nav

---
+++ src/components/data.jsx
@@ -0,0 +1,28 @@
+import { Overview, Campaign, Setting, Market } from "./icons";
+
+export const navLinkData = [
+    {
+        id: 1,
+        title: "Overview",
+        href: "/",
+        icon: Overview,
+    },
+    {
+        id: 2,
+        title: "Campaign",
+        href: "/campaigns",
+        icon: Campaign,
+    },
+    {
+        id: 3,
+        title: "Market Intelligence",
+        href: "/market",
+        icon: Market,
+    },
+    {
+        id: 4,
+        title: "Account Settings",
+        href: "/settings",
+        icon: Setting,
+    },
+];
--- src/components/layouts/SideNav.jsx
+++ src/components/layouts/SideNav.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import { Logo } from '../icons'
+import React from "react";
+import { Logo } from "../icons";
+import SearchBar from "../SearchBar";
+import Button from "../Button";
+import { Plus } from "../icons";
+import CustomNavLink from "../CustomNavLink";
+import { navLinkData } from "../data";
+import { Overview, Campaign } from "../icons";
 
 const SideNav = () => {
-  return (
-    <nav className='px-8 py-6 overflow-y-auto bg-[#F0F4F4] h-full'>
-      <Logo />
-    </nav>
-  )
-}
+    return (
+        <nav className="px-8 py-6 overflow-y-auto bg-[#F0F4F4] h-full">
+            <Logo />
 
-export default SideNav
+            <div className="mt-14 space-y-10">
+                <SearchBar variant="mobile" />
+                <Button
+                    variant="solid"
+                    size="lg"
+                    icon={<Plus />}
+                    colorScheme="primary"
+                >
+                    New Campaign
+                </Button>
+
+                <div className="">
+                    {navLinkData.map((link) => (
+                        <CustomNavLink
+                            key={link.id}
+                            title={link.title}
+                            href={link.href}
+                            icon={link.icon}
+                        />
+                    ))}
+                </div>
+            </div>
+            {/* <Button variant="outline" size="" colorScheme="red">New Campaign</Button>
+            <Button variant="solid" size="" colorScheme="red">New Campaign</Button>
+            <Button variant="outline" size="" colorScheme="black">New</Button> */}
+        </nav>
+    );
+};
+
+export default SideNav;
