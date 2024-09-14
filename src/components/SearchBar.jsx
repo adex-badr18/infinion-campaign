6898f0c06d068d90ee531fef09e6b653bdf8c589
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-09-14 10:32:31 +0100

    refactor: Make search bar responsive

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Search } from "./icons";
 
-const SearchBar = () => {
+const SearchBar = ({ variant }) => {
+    
     return (
-        <div className="relative border hidden md:block w-full md:w-60 lg:w-[391px] rounded-[4px]">
+        <div className={`relative border ${variant === "mobile" ? "md:hidden" : "hidden md:block"} w-full md:w-60 lg:w-[391px] rounded-[4px]`}>
             <input
                 type="text"
                 className="text-[#666666] w-full pl-[10px] pr-6 py-3 outline-none rounded-[4px]"
