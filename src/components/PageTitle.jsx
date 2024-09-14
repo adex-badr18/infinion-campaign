f5c9df84b286d82069299e4b696182b0a27e510f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-09-14 11:27:00 +0100

    feat: Create a reusable page title

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const PageTitle = () => {
-  return (
-    <div>PageTitle</div>
-  )
-}
+const PageTitle = ({ size, title }) => {
+    return (
+        <h1
+            className={`${
+                size === "sm"
+                    ? "text-[20px] font-semibold font-work-sans"
+                    : "text-2xl font-bold font-general-sans"
+            } text-primary`}
+        >
+            {title}
+        </h1>
+    );
+};
 
-export default PageTitle
+export default PageTitle;
