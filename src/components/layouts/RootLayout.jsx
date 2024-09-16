6b488e7cba6f1f3bdbdfdb86f2cae5f015128587
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-09-16 09:28:37 +0100

    feat: Add spinner & scrollToTop to layout

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import SideNav from "./SideNav";
 import TopNav from "./TopNav";
+import ScrollToTop from "../ScrollToTop";
+import Spinner from "../Spinner";
+import LayoutSpinnerModal from "./LayoutSpinnerModal";
 
 const RootLayout = () => {
+    const { pathname } = useLocation();
+    const { state } = useNavigation();
+    const isLoading = state === "loading";
+
     return (
         <div className="h-screen flex relative md:overflow-hidden">
+            <ScrollToTop dependency={pathname} />
+
             <aside className="hidden flex-none md:block w-[292px]">
                 <SideNav />
             </aside>
@@ -16,6 +25,10 @@ const RootLayout = () => {
                 <section className="grow px-4 sm:px-6 lg:px-[85px] py-8">
                     <Outlet />
                 </section>
+
+                <LayoutSpinnerModal isOpen={isLoading}>
+                    <Spinner />
+                </LayoutSpinnerModal>
             </main>
         </div>
     );
